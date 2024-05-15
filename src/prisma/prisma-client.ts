eb2bad46ba132a931767e4f6d711aa7da306af7b
Kevin Xu <40180478+kevinxu12@users.noreply.github.com>
2024-05-15 10:48:31 -0700

    Allow detail tracing to work with prisma

--- src/prisma/prisma-client.ts
+++ src/prisma/prisma-client.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import {prismaExtension} from '@detail-dev/trace';
 
 declare global {
   namespace NodeJS {
@@ -14,7 +15,7 @@ interface CustomNodeJsGlobal extends NodeJS.Global {
 // Prevent multiple instances of Prisma Client in development
 declare const global: CustomNodeJsGlobal;
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient().$extends(prismaExtension) as PrismaClient;
 
 if (process.env.NODE_ENV === 'development') {
   global.prisma = prisma;
